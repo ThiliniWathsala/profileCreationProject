b3950c623bf199e897cfb9be9a21fd01905b524d
ThiliniWathsala <o.t.wathsala@gmail.com>
2019-08-23 12:29:44 +0100

    add routing and navigarion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { ViewEmployeesComponent } from './view-employees/view-employees.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:UserComponent},
+  {path:'viewprofile', component:UserComponent },
+  {path:'viewemployees', component:ViewEmployeesComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+export const RoutingComponents=[UserComponent,
+                            ViewEmployeesComponent
+
+                            ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms'
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, RoutingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { UserServices } from './Services/user-service';
+import { EmployeesComponent } from './employees/employees.component';
+//import { ViewEmployeesComponent } from './view-employees/view-employees.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    RoutingComponents,
+    EmployeesComponent,
+  
   ],
   imports: [
     BrowserModule,
